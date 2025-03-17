9eab960bf394fe2e7a7d8a0c7ad4593b911a47ba
Jessekay0629 <168183588+Jessekay0629@users.noreply.github.com>
2025-03-17 14:00:14 +0200

    Update main.js

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,7 +1,7 @@
 // frontend/scripts/main.js
 document.getElementById('getMessageButton').addEventListener('click', async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/message');
+      const response = await fetch('https://testing-app-8en3.onrender.com/api/message');
       const data = await response.json();
       document.getElementById('message').textContent = data.message;
     } catch (error) {
